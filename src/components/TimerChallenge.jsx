327f8486da29c700ad96cd6ac6dcf627a2b7a698
MHA2001 <mha.akhoudndi@gmail.com>
2025-08-29 08:27:07 +0330

    feat: creating the TimerChallenge component basic structure

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,14 @@
+export default function TimerChallenge({ title, targetTime }) {
+	return (
+		<section className='challenge'>
+			<h2>{title}</h2>
+			<p className='challenge-time'>
+				{targetTime} second{targetTime > 1 ? 's' : ''}
+			</p>
+			<div>
+				<button>Start Challenge</button>
+			</div>
+			<p className=''>Time is running.... / Timer inactive</p>
+		</section>
+	);
+}
